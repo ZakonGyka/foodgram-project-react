7994a9df8cb912cc13e9b17429b2b77d672bef7a
zakongyka <zakongyka@yandex.ru>
2022-11-15 21:30:49 +0100

    fix users model & PEP8

--- backend/users/models.py
+++ backend/users/models.py
@@ -7,35 +7,29 @@ from django.db import models
 class User(AbstractUser):
     username_validator = UnicodeUsernameValidator()
     email = models.EmailField(
-        verbose_name='адрес электронной почты',
+        verbose_name='Адрес электронной почты',
         blank=False,
         unique=True,
-        max_length=254,
+        max_length=150,
         error_messages={
             'unique': 'Такой адрес электронной почты уже зарегистрирован.'
         },
     )
     username = models.CharField(
-        verbose_name='логин',
+        verbose_name='Логин',
         max_length=150,
         unique=True,
-        help_text='Не более 150 символов.',
         validators=[username_validator],
         error_messages={
-            'unique': 'Пользователь с таким именем уже зарегистрирован.'
+            'unique': 'Пользователь с таким логином уже зарегистрирован.'
         },
     )
-    first_name = models.CharField(verbose_name='имя', max_length=150)
-    last_name = models.CharField(verbose_name='фамилия', max_length=150)
+    first_name = models.CharField(verbose_name='Имя', max_length=150)
+    last_name = models.CharField(verbose_name='Фамилия', max_length=150)
     is_superuser = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
     USERNAME_FIELD = 'email'
-
-    # def save(self, *args, **kwargs):
-    #     if self.username == 'me':
-    #         return ValidationError('Username не может быть "me".')
-    #     super().save(*args, **kwargs)
 
     @property
     def is_admin(self):
@@ -54,12 +48,6 @@ class User(AbstractUser):
         ordering = ('username',)
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
-        # constraints = [
-        #     models.CheckConstraint(
-        #         check=~models.Q(username='me'),
-        #         name='Пользователь не может быть назван me!',
-        #     )
-        # ]
 
 
 class Follow(models.Model):
